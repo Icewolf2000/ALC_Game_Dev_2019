839b0788fc106afe4ced13c36e8fee692ba6b580
Unknown <cydney.n191@stu.nebo.edu>
2018-09-07 10:32:28 -0600

    Added Jump Function

--- 2D Game/Assets/Scripts/Character_Move.cs
+++ 2D Game/Assets/Scripts/Character_Move.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class Character_Move : MonoBehaviour {
 
-	public int moveSpeed = 10;
 
+	// Player Movement Variables
+	public int MoveSpeed;
+	public float JumpHeight;
+
+	//Player Grounded Variables
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask WhatIsGround;
+	private bool grounded;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
+	
+	void FixedUpdate () {
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, WhatIsGround);
+	}
+
 	// Update is called once per frame
 	void Update () {
-		
+
+		//Code makes charater jump
+		if(Input.GetKeyDown (KeyCode.Space)&& grounded){
+			Jump();
+		}
+	
+	}	
+	
+	public void Jump(){
+		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
+
 	}
+
+	
 }
+
