888c2dd5b40cc5185d3e98f10d7692591f295cee
Unknown <cydney.n191@stu.nebo.edu>
2018-09-27 10:25:07 -0600

    Created a moving enemy, fixed player disappearance.

---
+++ 2D Game/Assets/Scripts/Enemy_Patrol.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Patrol : MonoBehaviour {
+
+	//Movement Variables
+	public float MoveSpeed;
+	public bool MoveRight;
+
+	//Wall Check
+	public Transform WallCheck;
+	public float WallCheckRadius;
+	public LayerMask WhatIsWall;
+	private bool HittingWall;
+
+	//Edge Check
+	private bool NotAnEdge;
+	public Transform EdgeCheck;
+
+
+	// Update is called once per frame
+	void Update () {
+		NotAnEdge = Physics2D.OverlapCircle(EdgeCheck.position, WallCheckRadius, WhatIsWall);
+		
+		HittingWall = Physics2D.OverlapCircle(WallCheck.position, WallCheckRadius, WhatIsWall);
+
+		if(HittingWall || !NotAnEdge){
+			MoveRight = !MoveRight;
+		}
+		
+		if (MoveRight){
+			transform.localScale = new Vector3(-0.544f,0.509f,1f);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+		else {
+			transform.localScale = new Vector3(0.544f,0.509f,1f);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+	}
+}
