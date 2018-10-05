15bebb64246f4df8175c38d6d747d9c99b277b48
Unknown <cydney.n191@stu.nebo.edu>
2018-10-05 10:27:48 -0600

    Added Kill_PC script

---
+++ 2D Game/Assets/Scripts/Kill_PC.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kill_PC : MonoBehaviour {
+
+	public Level_Manager Level_Manager;
+
+	// Use this for initialization
+	void Start () {
+		Level_Manager = FindObjectOfType<Level_Manager>();
+	
+	}
+	
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.name == "PC"){
+			Level_Manager.RespawnPlayer();
+		}
+		
+	}
+}
