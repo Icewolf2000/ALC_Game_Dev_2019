65fba35abd9a774505a6bc2a20e3dca7bf1f87d4
Unknown <cydney.n191@stu.nebo.edu>
2018-10-17 10:39:18 -0600

    Fixed enemy death

--- 2D Game/Assets/Scripts/Character_Movement.cs
+++ 2D Game/Assets/Scripts/Character_Movement.cs
@@ -58,6 +58,7 @@ public class Character_Movement : MonoBehaviour {
 
 		GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
 
+		//Player Flip
 		if (GetComponent<Rigidbody2D>().velocity.x > 0)
 			transform.localScale = new Vector3(0.2f,0.2f,1f);
 
--- 2D Game/Assets/Scripts/Projectile.cs
+++ 2D Game/Assets/Scripts/Projectile.cs
@@ -18,12 +18,6 @@ public class Projectile : MonoBehaviour {
 
 		if(PC.transform.localScale.x < 0)
 			Speed = -Speed;
-
-			// Speed = Speed * Mathf.Sign(PC.transform.localScale.x);
-
-			// GetComponent<Rigidbody2D>().velocity = new Vector2(Speed + (PC.GetComponent<Rigidbody2D>().velocity.x/3),GetComponent<Rigidbody2D>().velocity.y + (PC.GetComponent<Rigidbody2D>().velocity.y/3));
-	
-	
 	}
 	
 	// Update is called once per frame
@@ -35,6 +29,7 @@ public class Projectile : MonoBehaviour {
 		if(other.tag == "Enemy"){
 			Instantiate(EnemyDeath, other.transform.position, other.transform.rotation);
 			ScoreManager.AddPoints (PointsForKill);
+			Destroy (other.gameObject);
 		}
 
 		Instantiate(ProjectileParticle, transform.position, transform.rotation);
